64ba7cb0129625fe1dec0ad615e40ab04749e803
weihai.tang <weihai.tang@saybot.com>
2015-08-31 19:51:48 +0800

    update

--- app/controllers/livedata_controller.rb
+++ app/controllers/livedata_controller.rb
@@ -1,15 +1,21 @@
 class LivedataController < ApplicationController
-  @data
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+  @@data = 10
 
   def index 
     #render nothing: true
-    create
-    render :json => @data
+    #create
+    #@data = rand 30
+    puts "-------"
+    puts @@data
+    render :json => @@data
   end
   
   def create
-    @data = rand 20
-    
+    puts "=============="    
+    @@data = params["cpu"]
+    #@data = rand 20
+    #redirect_to index
   end
 
 
